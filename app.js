e0e860176a910d3c8651c16832b196d54e7f26d2
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-08 09:49:21 +0600

    fix the image padding bug

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ const categoryContainer = document.getElementById("categoryContainer");
 
 const plantsContainer = document.getElementById("plantsContainer");
 
+
+
+const manageaSpinner = (status) => {
+  if(status === true){
+    document.getElementById("spinner").classList.remove("hidden")
+    document.getElementById("plantsContainer").classList.add("hidden")
+  }else{
+    document.getElementById("plantsContainer").classList.remove("hidden")
+    document.getElementById("spinner").classList.add("hidden")
+  }
+}
+
+
+
+
 const loadCategory = () => {
+ 
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
     .then((data) => {
@@ -16,13 +32,14 @@ const loadCategory = () => {
 };
 
 const showCategory = (categories) => {
+     
   categories.forEach((cat) => {
     categoryContainer.innerHTML += `
         <li onclick='clickCategory(${cat.id})' id="${cat.id}" class="hover:bg-[#1b8b48] hover:text-white cursor-pointer p-3 rounded-xl ">${cat.category_name}</li>
         `;
   });
   categoryContainer.addEventListener("click", (e) => {
-    
+
     const allli = document.querySelectorAll("li");
     allli.forEach((li) => {
       li.classList.remove("bg-[#1b8b48]","text-white");
@@ -41,6 +58,7 @@ const clickCategory = (id) => {
 };
 
 const loadPlantsByCategory = (id) => {
+  manageaSpinner(true)
   console.log(id);
   const url = id
     ? `https://openapi.programming-hero.com/api/category/${id}`
@@ -62,22 +80,29 @@ const showPlantsByCategory = (plant) => {
   plant.forEach((plants) => {
     plantsContainer.innerHTML += `
     
-    <div class="card bg-base-100 shadow-sm ">
-     <figure class = "lg:h-80 h-60  " >
-     <div><img class= "rounded-xl py-5" src = "${plants.image}"</div>
+    <div class="card bg-base-100 shadow-sm p-5">
+     <figure class = "lg:h-60 md:h-50 h-60 rounded-xl " >
+     <div><img class= " " src = "${plants.image}"</div>
     </figure>
   <div class="card-body">
     <h2 class="card-title">${plants.category}</h2>
     <p>${plants.description}</p>
     <div class="card-actions justify-end">
-      <button class = "w-full bg-[#15803D] text-center text-white p-3 rounded-xl">Add to card</Button>
+      <button class = "w-full bg-[#15803D] text-center text-white p-3 rounded-xl crt-btn hover:bg-[#CFF0DC] hover:text-gray-700 cursor-pointer">Add to card</Button>
     </div>
   </div>
 </div>
     
     `;
   });
+
+  manageaSpinner(false)
 };
+
+
+
+
+
 
 loadPlantsByCategory();
 loadCategory();
