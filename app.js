988a9bb8a62dd44a64fdc661b1b1d043185a84be
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-08 15:16:53 +0600

    all functionality done with responsive and without bug

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const loadCategory = () => {
 const showCategory = (categories) => {
   categories.forEach((cat) => {
     categoryContainer.innerHTML += `
-        <li onclick='clickCategory(${cat.id})' id="${cat.id}" class="hover:bg-[#1b8b48] hover:text-white cursor-pointer p-2 gap-4 space-y-3 rounded-xl ">${cat.category_name}</li>
+        <li onclick='clickCategory(${cat.id})' id="${cat.id}" class="hover:bg-[#1b8b48] mb-4 hover:text-white cursor-pointer p-2 gap-4 space-y-3 rounded-xl ">${cat.category_name}</li>
         `;
   });
   categoryContainer.addEventListener("click", (e) => {
@@ -72,12 +72,12 @@ const plantsDisplayDetails = (plants) => {
      <h1 class="text-2xl font-bold mb-3">${plants.name}</h1>
      </div>
      
-     <img class = "h-96 w-full rounded-xl " src = "${plants.image}"
+     <img class = "h-96 w-full rounded-xl " src = "${plants.image}">
      </div>
 
-  <div class="card-body">
-    <h2><span class="text-xl font-medium">Category : </span>${plants.category}</h2>
-    <h2><span class="text-xl font-medium">Price : </span>${plants.price}</h2>
+  <div class="card-body text-lg ">
+    <h2 class="text-xl font-medium">Category : <span class="text-gray-500" >${plants.category}</span></h2>
+    <h2><span class="text-xl font-medium">Price : </span><i class="fa-solid fa-bangladeshi-taka-sign "></i> ${plants.price}</h2>
     <p><span class="text-xl font-medium">Description : </span> ${plants.description}</p>
   </div>
      </div>
@@ -104,25 +104,57 @@ const loadPlantsByCategory = (id) => {
     });
 };
 
+const addtocart = (price, name, image) => {
+  const rightCartContainer = document.getElementById("rightCartContainer");
+
+  const rightBox = document.createElement("div");
+
+  rightBox.classList.add("cart-item");
+  rightBox.innerHTML = `
+    <div class="flex justify-between items-center bg-[#CFF0DC] p-2 my-3 rounded-xl">
+      <div class="mt-4 ">
+        <p class="text-xl text-gray-500 font-medium">${name}</p>
+        <p class="item-price lg:text-xl text-lg font-medium"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${price}</p></div>
+      <div>
+        <i class="close-btn cursor-pointer fa-solid text-red-600 fa-xmark"></i>
+       </div>
+    </div>
+  `;
+
+  rightCartContainer.append(rightBox);
+
+  const total = document.getElementById("total");
+  total.innerText = parseInt(total.innerText) + parseInt(price);
+
+  const closeBtn = rightBox.querySelector(".close-btn");
+  closeBtn.addEventListener("click", () => {
+    const itemPrice = parseInt(rightBox.querySelector(".item-price").innerText);
+    total.innerText = parseInt(total.innerText) - itemPrice;
+    rightBox.remove();
+  });
+};
+
 const showPlantsByCategory = (plant) => {
   console.log(plant);
   plantsContainer.innerHTML = " ";
   plant.forEach((plants) => {
     plantsContainer.innerHTML += `
-    
     <div class="card bg-base-100 shadow-sm p-5">
-     <figure class = "lg:h-60 md:h-50 h-60 rounded-xl " >
-     <div><img class= " " src = "${plants.image}"</div>
-    </figure>
-  <div class="card-body">
-    <h2 onclick="loadPlantsDetails('${plants.id}')" class="card-title cursor-pointer">${plants.category}</h2>
-    <p>${plants.description}</p>
-    <div class="card-actions justify-end">
-      <button class = "w-full bg-[#15803D] text-center text-white p-3 rounded-xl crt-btn hover:bg-[#CFF0DC] hover:text-gray-700 cursor-pointer">Add to card</Button>
+     <figure class = "lg:h-60 md:h-50 h-60 rounded-xl" >
+       <div><img class="" src="${plants.image}" /></div>
+     </figure>
+     <div class="card-body">
+       <h2 onclick="loadPlantsDetails('${plants.id}')" class="card-title cursor-pointer text-xl font-medium">${plants.category}</h2>
+       <p class="text-gray-500">${plants.description}</p>
+       <div class="flex items-center w-full">
+         <h1 class="flex justify-start text-lg text-[#15803D] bg-[#CFF0DC] p-3 rounded-full"> Frute Tree</h1>
+         <p class="flex justify-end text-lg font-medium items-center"> <i class="fa-solid fa-bangladeshi-taka-sign"></i> <span id='plant-price'>${plants.price}</span></p>
+       </div>
+       <div class="card-actions justify-end">
+         <button onclick="addtocart('${plants.price}', '${plants.name}', '${plants.image}')" class="w-full bg-[#15803D] text-center text-white p-3 rounded-xl crt-btn hover:bg-[#CFF0DC] hover:text-gray-700 cursor-pointer">Add to cart</button>
+       </div>
+     </div>
     </div>
-  </div>
-</div>
-    
     `;
   });
 
